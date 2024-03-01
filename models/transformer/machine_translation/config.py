e5193c53be9c9b72eb95910bc633d3c6306a19e0
daniel <danielgigliotti99.dg@gmail.com>
2024-03-01 18:10:46 +0100

    updated config

--- models/transformer/machine_translation/config.py
+++ models/transformer/machine_translation/config.py
@@ -1,13 +1,13 @@
 
-EMBED_DIM = 512
+EMBED_DIM = 100
 NUM_ENCODERS = 2  # Number of encoder blocks
 NUM_DECODERS = 6  # Number of decoder blocks
 DROPOUT = 0.1
-HEADS = 8
+HEADS = 4
 D_FF = 500
 
 BATCH_SIZE = 64
-NUM_EPOCHS = 10
+NUM_EPOCHS = 20
 NUM_WORKERS = 4
 LEARNING_RATE = 2e-4
 MAX_SEQ_LEN = 350
