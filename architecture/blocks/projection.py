f182d4ebae8428035c96c88d5b52efd588fc17a7
ggldnl <danielgigliotti99.dg@gmail.com>
2024-03-31 20:21:16 +0200

    Removed ugly comments

--- architecture/blocks/projection.py
+++ architecture/blocks/projection.py
@@ -3,9 +3,12 @@ import torch
 
 
 class ProjectionLayer(nn.Module):
-    def __init__(self, d_model: int, vocab_size: int)-> None: # model dimension and the size of the output vocabulary
+    def __init__(self,
+                 d_model: int,
+                 vocab_size: int
+                 ) -> None:
         super().__init__()
-        # linear layer for projecting the feature space of `d_model` to the output space of `vocab_size`
+
         self.proj = nn.Linear(d_model, vocab_size)
 
     def forward(self, x):
